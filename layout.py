f3511d24fc1457467b9e8c9b9d05162e195b601e
marten2 <marten.folkertsma@gmail.com>
2015-02-17 11:08:55 +0100

    new_commit

--- layout.py
+++ layout.py
@@ -2,7 +2,7 @@ import HTML
 import appartement_scraper as data
 
 if __name__ == '__main__':
-    table_data = data.extract_pararius()#get_appartements()
+    table_data = get_appartements()#data.extract_pararius()#
     for i in table_data:
         i[6] = HTML.link('Link',i[6])
 
